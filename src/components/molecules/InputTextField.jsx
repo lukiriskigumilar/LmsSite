53d65fbd5af2ae53f82b1e92af2158e3a656b3ae
Luki riski <contactlukiriski@gmail.com>
2025-03-08 09:37:02 +0700

    input text file

--- src/components/molecules/InputTextField.jsx
+++ src/components/molecules/InputTextField.jsx
@@ -1,10 +1,10 @@
 import InputField from "../atoms/InputField";
 import Label from "../atoms/Label";
 
-function InputTextField({ id, txtLabel, value, type = "text", handleChange, placeholder,style="mt-[10px]"}) {
+function InputTextField({  id, txtLabel, value, type = "text", handleChange, placeholder,style="mt-[10px]"}) {
     return (
         <>   <div className={style}>
-            <Label nameLabel={id} txtLabel={txtLabel} />
+            <Label nameLabel={id} txtLabel={txtLabel}  />
             <InputField inputId={id} typeInput={type} value={value} name={id} handleChange={handleChange} placeholder={placeholder} />
         </div>
         </>
